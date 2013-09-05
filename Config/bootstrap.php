e71d03c54dcfb99d4aa931a1be9d52b0143f6876
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2013-09-05 15:43:43 -0700

    Conditionally load DebugKit. Closes #3

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -68,6 +68,6 @@ Cache::config('default', array(
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
-if (Configure::read('debug')) {
+if (php_sapi_name() !== 'cli' && Configure::read('debug') && in_array('DebugKit', App::objects('plugin'))) {
 	CakePlugin::load('DebugKit');
 }
